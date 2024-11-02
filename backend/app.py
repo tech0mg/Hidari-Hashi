f8ddc22671fdb8b77b07d469366b8d912429121d
mukagen-mac <tech0.mukainakano@gmail.com>
2024-11-02 18:41:58 +0900

    backend/app.py add

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from flask import jsonify
+import json
+from flask_cors import CORS
+
+from db_control import crud, mymodels
+
+import requests
+
+# Azure Database for MySQL
+# REST APIでありCRUDを持っている
+app = Flask(__name__)
+CORS(app)
+ 
+
+@app.route("/")
+def index():
+    return "<p>Flask top page!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
